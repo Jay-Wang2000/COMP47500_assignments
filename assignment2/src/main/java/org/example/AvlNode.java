4d659a557b82f3b4c84805b8f2d4dcbb37ec4f14
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2024-03-19 22:33:02 +0000

    clean up

--- assignment2/src/main/java/org/example/AvlNode.java
+++ assignment2/src/main/java/org/example/AvlNode.java
@@ -22,6 +22,8 @@ public class AvlNode<T> {
     public String toString() {
         return "AvlNode{" +
                 "value=" + value +
+                ", left=" + left +
+                ", right=" + right +
                 '}';
     }
 }
